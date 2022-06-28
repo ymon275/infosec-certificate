b75cc6ed8a69ec72174e79ad60f9deef7063fb25
Jean Morrison <beatsandbeets275@gmail.com>
2022-06-28 18:26:32 -0500

    added HTTPS

--- myApp.js
+++ myApp.js
@@ -7,7 +7,8 @@ app.use(helmet.frameguard({action: 'deny'}));
 app.use(helmet.xssFilter());
 app.use(helmet.noSniff());
 app.use(helmet.ieNoOpen());
-
+const ninetyDaysInSeconds = 90*24*60*60;
+app.use(helmet.hsts({maxAge: ninetyDaysInSeconds, force: true}));
 
 
 
