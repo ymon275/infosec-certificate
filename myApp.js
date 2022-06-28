9e1b3ffe8d456a837878f3e2c599b6974222d5ed
Jean Morrison <beatsandbeets275@gmail.com>
2022-06-28 18:40:40 -0500

    added security policy

--- myApp.js
+++ myApp.js
@@ -11,7 +11,14 @@ const ninetyDaysInSeconds = 90*24*60*60;
 app.use(helmet.hsts({maxAge: ninetyDaysInSeconds, force: true}));
 app.use(helmet.dnsPrefetchControl());
 app.use(helmet.noCache());
-
+app.use(
+  helmet.contentSecurityPolicy({
+    directives: {
+      "defaultSrc": ["'self'"],
+      "scriptSrc": ["'self'", "trusted-cdn.com"],
+    },
+  })
+);
 
 
 
